72b618a4afbe18510d7ac93197a288fd5c0eadf8
RADHAKRISHNA KUCHI <149025551+201183@users.noreply.github.com>
2025-12-27 15:40:52 +0530

    Add guessing game with random number generation
    Implemented a guessing game that allows the user to guess a random number between 1 and 100 with a limited number of attempts.

--- Level-1/guessing_game.py
+++ Level-1/guessing_game.py
@@ -1,1 +1,16 @@
-
+import random
+# Generate random number
+number = random.randint(1, 100)
+# Number of attempts
+attempts = 5
+for i in range(attempts):
+    guess = int(input("Guess the number (1 to 100): "))
+    if guess > number:
+        print("Too High")
+    elif guess < number:
+        print("Too Low")
+    else:
+        print("Correct! You guessed the number.")
+        break
+else:
+    print("Game Over! The number was:", number)
